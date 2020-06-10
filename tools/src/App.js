5a11c0645487a2968a1624085d1987b8c33864fc
benacq <benacq44@gmail.com>
2020-06-10 04:18:12 +0100

    tools > added parcel, eslint, react and new pet file

---
+++ tools/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  render
+} from 'react-dom'
+import
+Pet
+from './Pet';
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cocktiel",
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mixed",
+    }),
+  ]);
+};
+
+render(React.createElement(App), document.getElementById("root"));
